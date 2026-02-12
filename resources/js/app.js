d235ef33453835af32f59531b54c738e7962f756
mirkobechini <mirkobechini@gmail.com>
2026-02-12 17:29:22 +0100

    Add Font Awesome dependency and import in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import '~resources/scss/app.scss';
 import '~icons/bootstrap-icons.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
